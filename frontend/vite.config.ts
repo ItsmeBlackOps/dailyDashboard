1716a9966efbad1f791fe811d07bed06beaf1e86
blackops <harikrushna09@gmail.com>
2025-06-27 04:42:33 -0400

    updated frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,19 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tsconfigPaths from 'vite-tsconfig-paths';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
 
-export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-  base: './',
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => ({
   server: {
-    port: 5173,
-    cors: true,
+    host: '::',
+    port: 8080,
+    open: true,
   },
-});
+  plugins: [react()].filter(Boolean),
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+}));
