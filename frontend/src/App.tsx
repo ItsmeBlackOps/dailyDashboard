f3ef9090c595ba18986430c20929116b2d6c0449
blackops <harikrushna09@gmail.com>
2025-07-03 23:46:48 +0530

    solved few issues with beep, corrected toasters and added users

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { TooltipProvider } from '@radix-ui/react-tooltip';
 import SignIn from './pages/auth/SignIn';
 import TasksToday from './pages/TasksToday';
+import { Toast } from './components/ui/toast';
 
 const queryClient = new QueryClient();
 
@@ -13,6 +14,7 @@ const App = () => (
         <Routes>
           <Route path="/auth/signin" element={<SignIn />} />
           <Route path="/dashboard" element={<TasksToday />} />
+          <Route path="/toast" element={<Toast />} />
           {/* Default to SignIn for any unmatched route */}
           <Route path="*" element={<SignIn />} />
         </Routes>
--- frontend/src/pages/auth/SignIn.tsx
+++ frontend/src/pages/auth/SignIn.tsx
@@ -19,7 +19,7 @@ export default function SignIn() {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState('');
   const navigate = useNavigate();
-  const API_URL = 'https://dailydb.tunn.dev/';
+  const API_URL = 'https://s02lbgvv-3004.inc1.devtunnels.ms/';
   
   const socket: Socket = useMemo(
     () => io(API_URL, { autoConnect: false, transports: ['websocket'] }),
